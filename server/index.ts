bce2d09a4d99d97c15e8fef150ed89f4a5f0d423
takanakahiko <takanakahiko@gmail.com>
2019-06-30 22:58:59 +0900

    Add getting conpass information endpoint to server(for debug)

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,31 @@
-import * as express from 'express'
-import * as passport from './twitter-oauth'
+import express from 'express'
+import passport from './twitter-oauth'
+import connpass from './connpass'
 
 const app = express();
 app.use(require('express-session')({ secret: 'some secret' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.set('json spaces', 2)
+
+app.get('/test', async (req, res) => res.send(await connpass()));
 app.get('/hello', (req, res) => res.send('world'));
+app.get('/user', (req, res) => res.json({ user: req.user }));
 
 // auth
-app.get('/auth/twitter', passport.authenticate('twitter'));
-app.get('/auth/twitter/callback', passport.authenticate('twitter'), (req, res) => {
-  res.json({ user: req.user });
-});
+app.get('/login', passport.authenticate('twitter'));
+app.get('/login/callback', passport.authenticate('twitter', {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
 app.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
 });
 
 module.exports = {
-  path: '/server',
+  path: '/api',
   handler: app,
 };
