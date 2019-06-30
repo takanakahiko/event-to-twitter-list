9f379846a876d956f720bc3bc029a0b90b6c41ea
takanakahiko <takanakahiko@gmail.com>
2019-07-01 01:33:52 +0900

    iroiro

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,85 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github-circle"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
+  <section class="hero is-light is-fullheight is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Event to Twitter List
+        </h1>
 
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
+        <b-field label="Login">
+          <div class="buttons">
+            <a href="/api/login" class="button is-info" :disabled="isLogin">
+              Twitter Login
+            </a>
+            <a href="/api/logout" class="button is-danger" :disabled="!isLogin">
+              Logout
+            </a>
+          </div>
+        </b-field>
 
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
+        <b-field label="Event url">
+          <b-input v-model="eventUrl" :disabled="!isLogin" />
+        </b-field>
 
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+        <b-field label="List name">
+          <b-input v-model="listName" :disabled="!isLogin" />
+        </b-field>
+
+        <b-field class="is-grouped">
+          <b-button class="button is-link" :disabled="!isLogin" @click="submit">
+            Submit
+          </b-button>
+        </b-field>
+      </div>
     </div>
   </section>
 </template>
 
-<script>
-import Card from '~/components/Card'
+<script lang="ts">
+import Vue from 'vue'
 
-export default {
-  name: 'HomePage',
-
-  components: {
-    Card
+export default Vue.extend({
+  data() {
+    return {
+      userInfo: null,
+      isStepsClickable: false,
+      eventUrl: 'https://prickathon.connpass.com/event/132723',
+      listName: 'プリッカソン7'
+    }
+  },
+  computed: {
+    isLogin() {
+      return this.$data.userInfo != null
+    }
+  },
+  async mounted() {
+    const ret = await fetch(`${process.env.baseUrl}/api/user`)
+    const data = await ret.json()
+    // eslint-disable-next-line
+    console.log(data)
+    this.userInfo = (data.user !== {}) ? data.user : null
+  },
+  methods: {
+    async submit() {
+      const postData = {
+        eventUrl: this.eventUrl,
+        listName: this.listName
+      }
+      const ret = await fetch(`${process.env.baseUrl}/api/create`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify(postData)
+      })
+      const data = await ret.json()
+      if (data.status === 'succeed') {
+        this.$toast.open({ message: '作成しました!', type: 'is-success' })
+      } else {
+        this.$toast.open({ message: '失敗しました', type: 'is-danger' })
+      }
+    }
   }
-}
+})
 </script>
