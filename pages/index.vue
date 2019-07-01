31be73134de9b331281ca5d40472fd64bdb032a4
takanakahiko <takanakahiko@gmail.com>
2019-07-01 16:58:10 +0900

    なんなん....

--- pages/index.vue
+++ pages/index.vue
@@ -78,10 +78,10 @@ export default Vue.extend({
       const data = await ret.json()
       if (data.status === 'succeed') {
         // eslint-disable-next-line
-        this.$toast.open({ message: '作成しました!', type: 'is-success' })
+        (this as any).$toast.open({ message: '作成しました!', type: 'is-success' })
       } else {
         // eslint-disable-next-line
-        this.$toast.open({ message: '失敗しました', type: 'is-danger' })
+        (this as any).$toast.open({ message: '失敗しました', type: 'is-danger' })
       }
     }
   }
