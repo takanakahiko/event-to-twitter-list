a15b4645797ce87b3f7c041fcfbd6ee958a8bbee
takanakahiko <takanakahiko@gmail.com>
2019-07-02 21:38:24 +0900

    fix: add option to express-session( remove deplicated warn )

--- server/twitter-oauth.ts
+++ server/twitter-oauth.ts
@@ -27,7 +27,9 @@ export const expressWithTwitterOauth = () => {
   })
 
   const app = express()
-  app.use(require('express-session')({ secret: 'some secret' }))
+  app.use(require('express-session')({
+    secret: 'some secret', resave: false, saveUninitialized: true
+  }))
   app.use(passport.initialize())
   app.use(passport.session())
 
