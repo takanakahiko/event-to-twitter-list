f4410e9ad6e9956bdfa6f539cf9a7238debd99ae
takanakahiko <takanakahiko@gmail.com>
2019-07-03 17:31:52 +0900

    feat: set page title & google anal setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ const config: NuxtConfiguration = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'イベントページからツイッターのリストを作るやつ',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -30,6 +30,7 @@ const config: NuxtConfiguration = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '~plugins/ga.js', ssr: false }
   ],
   /*
   ** Nuxt.js modules
