5fa73cd3b7b06a689c0c4649d174838984a445f1
takanakahiko <takanakahiko@gmail.com>
2019-07-09 21:27:24 +0900

    feat: setting OGP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,13 @@ const config: NuxtConfiguration = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'description', name: 'description', content: 'connpass の URL から Twitter のリストを自動生成します' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'イベントページからツイッターのリストを作るやつ' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://event-to-twitter-list.herokuapp.com/ogp.png' },
+      { hid: 'og:title', property: 'og:title', content: 'イベントページからツイッターのリストを作るやつ' },
+      { hid: 'og:description', property: 'og:description', content: 'connpass の URL から Twitter のリストを自動生成します' },
+      { hid: 'og:image', property: 'og:image', content: 'https://example.com/img/ogp/common.jpg' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
